07b7385592e4aedfbb93e97e9a715d519626a3a8
Sneha <sneha.susan@perfomatix.com>
2018-09-17 22:23:58 +0530

    Array spread operator
    - instead of concat and also swap the order

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-function sumAll(message:string, ...arr:number[]){
-    console.log(arguments);
-    return arr.reduce((prev,next) => prev + next)
-}
+const toppings = ['chilli', 'pepper'];
 
-const sum = sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+const newToppings = ['pepperoni'];
 
-console.log(sum);
+const allToppings = [...toppings, ... newToppings];
+
+console.log(allToppings)
