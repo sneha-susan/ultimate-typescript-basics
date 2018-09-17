2d5dc15e638d950eb4124f342ae9fdf0372e99f9
Sneha <sneha.susan@perfomatix.com>
2018-09-17 20:44:00 +0530

    arrow functions
    1. function keyword
    2. arrow function
    3. this
    4. implicit returns
    5. resolving scope issues obtained this when using function keyword and resolved it with arrow function

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Ahoy TypeScript!');
+const pizzas = [{
+    name : 'Pepporoni', toppings: ['pepporoni']
+}];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase()) // implicit return function
+
+console.log(mappedPizzas);
+
+const pizza ={
+    name : 'Blazing Inferno',
+    getName : ()=> pizza.name
+}
+
+console.log(pizza.getName())
