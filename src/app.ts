0fa69522a9b01bdb4c22001d8736f80d1ee453a9
Sneha <sneha.susan@perfomatix.com>
2018-09-18 21:38:34 +0530

    String Type

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-const pizzaCost: number = 10;
-const pizzaToppings: number = 2;
+const coupon : string ='pizza25';
 
-function calculatePrice(cost: number, toppings: number): number{
-    return cost + 1.5*toppings;
+function normalizeString(code: string) : string{
+    return code.toUpperCase();
 }
 
-const cost = calculatePrice(pizzaCost, pizzaToppings);
+const couponMessage : string = `Final coupon is ${normalizeString(coupon)}`;
+
+console.log(couponMessage);
