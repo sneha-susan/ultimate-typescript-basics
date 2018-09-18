0371972f81d9050ecca653c13ceb2d71263f2d06
Sneha <sneha.susan@perfomatix.com>
2018-09-18 21:21:48 +0530

    Number types

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,8 @@
+const pizzaCost: number = 10;
+const pizzaToppings: number = 2;
 
-//destructuring object
-
-const pizza = {
-    name: 'Pepperoni',
-    toppings: ['pepperoni']
+function calculatePrice(cost: number, toppings: number): number{
+    return cost + 1.5*toppings;
 }
 
-function order({name: pizzaName, toppings: pizzaToppings}: any){
-    return {pizzaName, pizzaToppings}
-}
-
-const {pizzaName} = order(pizza);
-
-
-//destructuring arrays
-
-const toppings = ['pepperoni', 'chilli', 'bacon']
-
-const [ first, second, third ] =  toppings;
-
-console.log(first, second, third);
-
-function logToppings([first, second, third] : any){
-    console.log(first, second, third)
-}
-
-logToppings(toppings);
+const cost = calculatePrice(pizzaCost, pizzaToppings);
