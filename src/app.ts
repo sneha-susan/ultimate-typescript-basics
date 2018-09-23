3b8336b6737c40b9f9096d05402156804efa44bb
Sneha <sneha.susan@perfomatix.com>
2018-09-23 20:43:07 +0530

    Setters and Getters (Accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+class Sizes{
+    constructor(public sizes: string[]){}
+
+    set availableSizes(sizes: string[]){
+        this.sizes = sizes;
+    }
+
+    get availableSizes(){
+        return this.sizes;
+    }
+}
+
+const sizes = new Sizes(['small', 'medium']);
+console.log(sizes.availableSizes) //invoke getter
+sizes.availableSizes = ['medium', 'large'];   //invoke the setter
+console.log(sizes.availableSizes) //invoke getter after the change
+
 class Pizza {
      public toppings : string[] = [];
 
