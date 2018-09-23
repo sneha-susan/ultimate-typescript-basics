b5859e2dafc4542ca14c3b4a04f2ea9132c98704
Sneha <sneha.susan@perfomatix.com>
2018-09-23 20:26:20 +0530

    Readonly members

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Pizza {
      public toppings : string[] = [];
 
-     constructor(private name: string){   }
+     constructor(readonly name: string){   }
 
      public addTopping(topping: string){
          this.toppings.push(topping);
@@ -11,5 +11,6 @@ class Pizza {
 const pizza = new Pizza('Pepperoni');
 
 pizza.addTopping('pepperoni');
+console.log(pizza.name);   
 
 console.log(pizza);   
