fdb895de5e4d6dfa1aba566f2b0abb60248d4464
Sneha <sneha.susan@perfomatix.com>
2018-09-23 22:01:58 +0530

    -Static properties and methods:

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,10 @@
-interface SizesInterface{
-    availableSizes : string[]
-}
-
-abstract class Sizes implements SizesInterface{
-    constructor(protected sizes: string[]){}
-
-    set availableSizes(sizes: string[]){
-        this.sizes = sizes;
-    }
-
-    get availableSizes(){
-        return this.sizes;
+class Coupon{
+    static allowed = ['Pepperoni', 'Blazing Inferno'];
+    static create(percentage: number){
+        return `PIZZA_RESTAURANT_${percentage}`;
     }
 }
 
-interface PizzaInterface extends SizesInterface{
-    readonly name : string;
-    toppings: string[];
-    updateSizes(sizes: string[]): void;
-    addTopping(topping: string): void;
-}
+console.log(Coupon.allowed);
 
-class Pizza extends Sizes implements PizzaInterface{
-     public toppings : string[] = [];
-
-     constructor(readonly name: string, sizes: string[]){ 
-         super(sizes);
-       }
-    public updateSizes(sizes: string[]){
-        this.sizes = sizes;
-    }
-
-     public addTopping(topping: string){
-         this.toppings.push(topping);
-     }
-}
-
-const pizza = new Pizza('Pepperoni', ['small', 'medium']);
-console.log(pizza.availableSizes);  //invoke getter
-
-pizza.updateSizes(['large']);
-console.log(pizza.availableSizes);  //invoke getter after the change
+console.log(Coupon.create(25));
